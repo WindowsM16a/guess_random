59ea2015e8f78e1353b04ae3963b7284339623ce
WindowsM16a <wuvershayne@gmail.com>
2025-03-06 20:49:01 +0000

    fixed the constant computer guess

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ const min = 1;
 const max = 10;
 var guess;
 
-//TODO: make this dynamic so i get new values each submit button
-const randNum = Math.random() * (min - max) + max;
-const randNumDecimals = randNum.toFixed(2);
+
+function randNumGen(min, max){
+    let randomNumber = Math.random() * (min - max) + max;
+    return randomNumber;
+}
 
 // event listener for enter key to submit user guess
 const form = document.querySelector('form');
+
 form.addEventListener('keydown', function(event){
     if (event.key === 13) {
         event.preventDefault();
@@ -22,23 +25,24 @@ form.addEventListener('keydown', function(event){
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-function revealNum(){
-    document.getElementById("revealbtn").childNodes[0].nodeValue=`${randNumDecimals}`;
-    sleep(800).then(() => {
-        document.getElementById("revealbtn").childNodes[0].nodeValue="Reveal";
-    })
-    
-}
+// TODO: make the reveal button work
+// function revealNum(){
+//     document.getElementById("revealbtn").childNodes[0].nodeValue=`${randNumDecimals}`;
+//     sleep(800).then(() => {
+//         document.getElementById("revealbtn").childNodes[0].nodeValue="Reveal";
+//     })
+// }
 
 // main logic for the game
 function mainGame(){
 
+    let randNumDecimals = randNumGen(min, max).toFixed(2);
+
     console.log(`random number = ${randNumDecimals}`);
 
     guess = document.getElementById("guess").value;
+
     console.log(`user guess = ${guess}`);
 
     guess == randNumDecimals ? alert("You win!") : alert(`You lose! The number was ${randNumDecimals}`);
-
 }
