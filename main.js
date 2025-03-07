a731ad9bc65653bfbf85f348872fa59a5cd7fd12
WindowsM16a <wuvershayne@gmail.com>
2025-03-07 13:02:24 +0000

    fixed the fucking bug

--- main.js
+++ main.js
@@ -3,12 +3,41 @@
 // min and max value thresholds for the random numbers
 const min = 1;
 const max = 10;
-var guess;
+let guess;
+let randomNumber;
 
-
+// function to generate random number
 function randNumGen(min, max){
-    let randomNumber = Math.random() * (min - max) + max;
+    randomNumber = (Math.random() * (max - min) + min).toFixed(2);
     return randomNumber;
+}
+
+randNumGen(min, max);
+
+// main logic for the game
+function mainGame(){
+
+    console.log(`random number = ${randomNumber}`);
+
+    guess = document.getElementById("guess").value;
+
+    console.log(`user guess = ${guess}`);
+
+    guess == randomNumber ? alert("You win!") : alert(`You lose! The number was ${randomNumber}`);
+
+    randNumGen(min, max);
+}
+
+// logic to reveal the number and hide after 0.8 seconds
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function revealNum(){
+    document.getElementById("revealbtn").childNodes[0].nodeValue=`${randomNumber}`;
+    sleep(800).then(() => {
+        document.getElementById("revealbtn").childNodes[0].nodeValue="Reveal";
+    })
 }
 
 // event listener for enter key to submit user guess
@@ -20,29 +49,3 @@ form.addEventListener('keydown', function(event){
         mainGame();
     }
 });
-
-// logic to reveal the number and hide after 0.8 seconds
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-// TODO: make the reveal button work
-// function revealNum(){
-//     document.getElementById("revealbtn").childNodes[0].nodeValue=`${randNumDecimals}`;
-//     sleep(800).then(() => {
-//         document.getElementById("revealbtn").childNodes[0].nodeValue="Reveal";
-//     })
-// }
-
-// main logic for the game
-function mainGame(){
-
-    let randNumDecimals = randNumGen(min, max).toFixed(2);
-
-    console.log(`random number = ${randNumDecimals}`);
-
-    guess = document.getElementById("guess").value;
-
-    console.log(`user guess = ${guess}`);
-
-    guess == randNumDecimals ? alert("You win!") : alert(`You lose! The number was ${randNumDecimals}`);
-}
