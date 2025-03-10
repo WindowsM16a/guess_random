b9e897a6db0f4b7c869b237ade91b45d589c9fa2
WindowsM16a <wuvershayne@gmail.com>
2025-03-10 22:05:54 +0000

    implemented logic to hide and reveal the reveal button when it should be hidden and when not

--- main.js
+++ main.js
@@ -3,38 +3,28 @@
 // min and max value thresholds for the random numbers
 const min = 1;
 const max = 10;
-let guess;
+const form = document.getElementById('guessForm');
+let guess
 let randomNumber;
+let tries = 3;
 
 // function to generate random number
 function randNumGen(min, max){
     randomNumber = (Math.random() * (max - min) + min).toFixed(2);
+    tries = 3;
     return randomNumber;
 }
 
 // initial call to generate random number
 randNumGen(min, max);
 
-// main logic for the game
-function mainGame(){
-
-    console.log(`random number = ${randomNumber}`);
-
-    guess = document.getElementById("guess").value;
-
-    console.log(`user guess = ${guess}`);
-
-    // validate user input
-    if(!validateInput(guess)){
-        alert("Please enter a decimal number from 1.00 to 10.00 with 2 decimal places.");
-        return;
+// event listener for enter key to submit user guess
+form?.addEventListener('keydown', function(event){
+    if (event.key === 13) {
+        event.preventDefault();
+        mainGame();
     }
-
-    guess == randomNumber ? alert("You win!") : alert(`You lose! The number was ${randomNumber}`);
-
-    // call to generate a new random number after every instance of the main game function
-    randNumGen(min, max);
-}
+});
 
 // logic to validate the user input
 function validateInput(input){
@@ -42,25 +32,55 @@ function validateInput(input){
     return regex.test(input);
 }
 
-
 // logic to reveal the number and hide after 0.8 seconds
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function revealNum(){
-    document.getElementById("revealbtn").childNodes[0].nodeValue=`${randomNumber}`;
+    console.log('hello')
+    const button = document.getElementById('revealbtn');
+    
+    button.innerText = `${randomNumber}`;
     sleep(800).then(() => {
-        document.getElementById("revealbtn").childNodes[0].nodeValue="Reveal";
+        button.innerText="Reveal";
     })
 }
 
-// event listener for enter key to submit user guess
-const form = document.querySelector('form');
+// main logic for the game
+function mainGame(){
 
-form.addEventListener('keydown', function(event){
-    if (event.key === 13) {
-        event.preventDefault();
-        mainGame();
+    const revealButton = document.getElementById('revealbtn')
+
+    if (tries == 2){
+        revealButton.style.display = 'inline-block'
     }
-});
+
+    // console.log(`random number = ${randomNumber}`);
+
+    guess = document.getElementById("guess").value;
+
+    // validate user input
+    if(!validateInput(guess)){
+        alert("Please enter a decimal number from 1.00 to 10.00 with 2 decimal places.");
+        return;
+    }
+
+    if (guess == randomNumber){
+        alert(`You win!`)
+        guess = ''
+        revealButton.style.display = 'none'
+
+        // call to generate a new random number after every correct guess
+        randNumGen(min, max);
+
+    } else if (tries > 1 && tries <= 3 ){
+        --tries;
+        alert(`Wrong guess! ${tries} ${tries == 1 ? 'try' : 'tries'} left`)
+        
+    } else {
+        alert('Game Over')
+        randNumGen(min, max)
+        revealButton.style.display = 'none'
+    }
+}
